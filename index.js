be0736a89e928836ead24e0fc261739aa2a86ab8
slowandrarecooked <airbendervineeth@gmail.com>
2023-09-15 16:58:45 +0530

    v1

--- index.js
+++ index.js
@@ -195,7 +195,7 @@ let resume2 = document
 
 function NewTab() {
   window.open(
-    "https://drive.google.com/file/d/1pg4PgIBrdNWXQpgm6w8KXwMdNRVDhNoL/view?usp=sharing",
+    "https://drive.google.com/file/d/1Z8j-U9CdL6SDPrNXFmZeMlSEaEEmW3Hr/view?usp=sharing",
     "_blank"
   );
 }
